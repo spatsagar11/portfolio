5ce75361dc6e6a5b4bc2b07355276af90afd4ef4
Sagar Patel <sagarspatel90@gmail.com>
2018-05-23 22:55:34 -0400

    Second commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var hasClassName = function(el, className) {
+  return !!~(' ' + el.className + ' ').indexOf(' ' + className + ' ');
+};
+
+var toggleClassName = function(el, className) {
+  (function toggleText(tagName, position, text) {
+    var saved = "creations";
+
+    if(!hasClassName(el, className)) {
+      el.className += ' ' + className;
+      document.getElementsByTagName(tagName, el)[position].innerHTML = text;
+      return;
+    }
+
+    el.className = el.className.replace(new RegExp('\\s*' + className + '\\s*'), ' ');
+    document.getElementsByTagName(tagName, el)[position].innerHTML = saved;
+  })('h3', 3, 'Social Me');
+};
+
+document.getElementById('create').addEventListener('click', function (e) {
+  e.preventDefault();
+  toggleClassName(document.getElementsByTagName('body')[0], 'transition');
+});
